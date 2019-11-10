0bea88d4fe5f729e9ceec040d78bfb40f2f76ec3
Ian Read <ianread@gmail.com>
2019-11-10 12:21:47 -0800

    Added new article

--- src/registry.js
+++ src/registry.js
@@ -1,7 +1,9 @@
 import TestArticle from "./Articles/TestArticle"
 import KnowingMagic from "./Articles/KnowingMagic"
+import Communication from "./Articles/Communication"
 
 const MyArticles = [
+    new Communication(),
     new KnowingMagic()
 ];
 
