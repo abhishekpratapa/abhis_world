0a53ed1cd904efe48d9d32450430a9a995058d3a
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-11-13 20:30:46 -0800

    Added statue of David Article

--- src/registry.js
+++ src/registry.js
@@ -1,8 +1,10 @@
 import TestArticle from "./Articles/TestArticle"
 import KnowingMagic from "./Articles/KnowingMagic"
 import Communication from "./Articles/Communication"
+import StatueOfDavid from "./Articles/StatueOfDavid"
 
 const MyArticles = [
+	new StatueOfDavid(),
     new Communication(),
     new KnowingMagic()
 ];
