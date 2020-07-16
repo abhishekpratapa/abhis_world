201527fbfcac27957a22a44725e22c82e5b40596
Abhishek Pratapa <abhishekpratapa@gmail.com>
2020-07-15 23:00:38 -0700

    docs folder

---
+++ docs/precache-manifest.bfe5a97eabaa358e63bd72c76241e627.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ff1cea790e439d91a178078ae034a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a99321981afcc02ef96",
+    "url": "/static/css/main.0a670f1a.chunk.css"
+  },
+  {
+    "revision": "cf55aa277c868c2ebb38",
+    "url": "/static/js/2.f1a75b1a.chunk.js"
+  },
+  {
+    "revision": "6a99321981afcc02ef96",
+    "url": "/static/js/main.83fa1c92.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "fc028c12a5a80e091f748747c76da3b8",
+    "url": "/static/media/audio_alignment.fc028c12.png"
+  },
+  {
+    "revision": "7f1cb658f14933cba6a4f12cd788cf4e",
+    "url": "/static/media/blog.7f1cb658.svg"
+  },
+  {
+    "revision": "d270a9993cf140ce914c141114049f29",
+    "url": "/static/media/books.d270a999.svg"
+  },
+  {
+    "revision": "54355055395c4caf39b35ab5095880f9",
+    "url": "/static/media/calendar.54355055.svg"
+  },
+  {
+    "revision": "e9300ef37d263ed0482ec2188ef5a551",
+    "url": "/static/media/example.e9300ef3.jpg"
+  },
+  {
+    "revision": "330d4153eda242ee15c6fbd929314a8a",
+    "url": "/static/media/face.330d4153.svg"
+  },
+  {
+    "revision": "39fe867837e1ce24ceabc40460293d12",
+    "url": "/static/media/facebook.39fe8678.svg"
+  },
+  {
+    "revision": "bf6c67c154b69729e67ea76e62103c29",
+    "url": "/static/media/leaf.bf6c67c1.svg"
+  },
+  {
+    "revision": "162211c0f9aa674234ed67c5ca7b4ab3",
+    "url": "/static/media/lightbulb.162211c0.svg"
+  },
+  {
+    "revision": "a77743470db2bbd32b03916f2b43f89c",
+    "url": "/static/media/lightbulb.a7774347.svg"
+  },
+  {
+    "revision": "c0edce7ae03733c5f7a2dcda4b7e6e18",
+    "url": "/static/media/logo.c0edce7a.svg"
+  },
+  {
+    "revision": "b8da770aaf590c1c4bb3052923e5addc",
+    "url": "/static/media/mythical_man_month.b8da770a.png"
+  },
+  {
+    "revision": "a22fcd01ee22c0c4f01581d8ab098b95",
+    "url": "/static/media/one_day_builds.a22fcd01.svg"
+  },
+  {
+    "revision": "a51026b2f08125bb2c599db4cba58d5e",
+    "url": "/static/media/plants.a51026b2.svg"
+  },
+  {
+    "revision": "7d5fa039000e7ffb5b7cbfd5a4331733",
+    "url": "/static/media/projects.7d5fa039.svg"
+  },
+  {
+    "revision": "ab06d77cd1febaf60ebc4e421a4ed99d",
+    "url": "/static/media/projects.ab06d77c.svg"
+  },
+  {
+    "revision": "c740711970f90bdc36489f9c9afac16c",
+    "url": "/static/media/right_arrow.c7407119.svg"
+  },
+  {
+    "revision": "2ee4ebabe1366a1fdbc8177a5f98ed2e",
+    "url": "/static/media/twitter.2ee4ebab.svg"
+  },
+  {
+    "revision": "f33124afdb272240374890d983bfa735",
+    "url": "/static/media/youtube.f33124af.svg"
+  }
+]);
